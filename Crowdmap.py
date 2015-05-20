59aa6fba29981bf1ace07766723fef63e9adf9c1
netanelbn <netanelbenda7@gmail.com>
2015-05-20 17:14:55 +0300

    git 4 mission

---
+++ Crowdmap.py
@@ -0,0 +1,7 @@
+class Crowdmap(object):
+	def __init__(self, init_list):
+		self.list = init_list
+	
+	def get_all_posts_for(self,name):
+		return [post for post in self.list if post.find(name) != -1]
+		
