335bff55843ca945a9e40ddab7156b7c8762d80f
netanelbn <netanelbenda7@gmail.com>
2015-05-20 17:22:53 +0300

    test mission 4

--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -3,4 +3,11 @@ from Crowdmap import Crowdmap
 class FindAPersonTests(unittest.TestCase):
 	def setUp(self):
 		self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Missing Cowboy"])
+			def test_getAllPostsForName(self):
+		posts = self.crowdmap.get_all_posts_for("Or")
+		self.assertEquals(posts,["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
+		
+	def test_getAllPostForMissingName(self):
+		posts = self.crowdmap.get_all_posts_for("Joe")
+		self.assertEquals([],posts)
 		
