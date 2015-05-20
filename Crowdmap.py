1b2fe36948c981dcf86cd29a41291d71ce1ab329
netanelbn <netanelbenda7@gmail.com>
2015-05-20 17:16:50 +0300

    mission 4

--- Crowdmap.py
+++ Crowdmap.py
@@ -5,3 +5,6 @@ class Crowdmap(object):
 	def get_all_posts_for(self,name):
 		return [post for post in self.list if post.find(name) != -1]
 		
+	def is_location_for_name(self,name):
+		posts = [post for post in self.list if post.find(name) != -1  and post.find("at") != -1]
+		return len(posts) != 0
